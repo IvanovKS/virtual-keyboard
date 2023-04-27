b124c15fdd96a5899c2f6bceaeb6a5753965f5e0
IvanovKS <Skitchi@mail.ru>
2023-04-27 14:14:00 +0300

    feat: add event listener for most buttons

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ const HTML = `
       <div class="keys">]
         <div class="sub-keys">}</div>
       </div>
-      <div class="keys slash">\
+      <div class="keys backslash">&#92;
         <div class="sub-keys">|</div>
       </div>
       <div class="keys delete">Del</div>
@@ -128,3 +128,151 @@ const HTML = `
 `;
 
 document.body.innerHTML = HTML;
+// -----------------------------------------------------------------------
+
+// * Variables -----------------------------------------------------------
+const KEYS = document.querySelectorAll('.keys');
+const BACKSPACE = document.querySelector('.backspace');
+const TAB = document.querySelector('.tab');
+const BACKSLASH = document.querySelector('.backslash');
+const DELETE = document.querySelector('.delete');
+const CAPS = document.querySelector('.caps');
+const ENTER = document.querySelector('.enter');
+const SHIFT_RIGHT = document.querySelector('.shift-right');
+const SHIFT_LEFT = document.querySelector('.shift-left');
+const UP = document.querySelector('.up');
+const DOWN = document.querySelector('.down');
+const LEFT = document.querySelector('.left');
+const RIGHT = document.querySelector('.right');
+const WIN = document.querySelector('.win');
+const SPACE = document.querySelector('.space');
+const ALT_LEFT = document.querySelector('.alt-left');
+const ALT_RIGHT = document.querySelector('.alt-right');
+const CTRL_LEFT = document.querySelector('.ctrl-left');
+const CTRL_RIGHT = document.querySelector('.ctrl-right');
+const FLAG = document.querySelector('.flag');
+const TEXT = document.querySelector('.text');
+// -----------------------------------------------------------------------
+
+// * Focus onload page----------------------------------------------------
+window.onload = () => {
+  TEXT.focus();
+}
+// -----------------------------------------------------------------------
+
+// * ??????????????----------------------------------------------------
+for (let i = 0; i < KEYS.length; i++) {
+  KEYS[i].setAttribute('keyValue', KEYS[i].innerText);
+  KEYS[i].setAttribute('lowerCaseValue', KEYS[i].innerText.toLowerCase());
+}
+
+window.addEventListener('keydown', function(el) {
+  for (let i = 0; i < KEYS.length; i++) {
+    if (el.key === KEYS[i].getAttribute('keyValue') || el.key === KEYS[i].getAttribute('lowerCaseValue')) {
+      KEYS[i].classList.add('active');
+    }
+    if (el.code === 'Space') {
+      SPACE.classList.add('active');
+    }
+    if (el.code === 'Delete') {
+      DELETE.classList.add('active');
+    }
+    if (el.code === 'ArrowUp') {
+      UP.classList.add('active');
+    }
+    if (el.code === 'ArrowDown') {
+      DOWN.classList.add('active');
+    }
+    if (el.code === 'ArrowLeft') {
+      LEFT.classList.add('active');
+    }
+    if (el.code === 'ArrowRight') {
+      RIGHT.classList.add('active');
+    }
+    if (el.code === 'Backslash') {
+      BACKSLASH.classList.add('active');
+    }
+    if (el.code === 'ShiftLeft') {
+      SHIFT_RIGHT.classList.remove('active');
+    }
+    if (el.code === 'ShiftRight') {
+      SHIFT_LEFT.classList.remove('active');
+    }
+    if (el.code === 'AltLeft') {
+      ALT_RIGHT.classList.remove('active');
+    }
+    if (el.code === 'AltRight') {
+      ALT_LEFT.classList.remove('active');
+    }
+    if (el.code === 'ControlLeft') {
+      CTRL_LEFT.classList.add('active');
+    }
+    if (el.code === 'ControlRight') {
+      CTRL_RIGHT.classList.add('active');
+    }
+    if (el.code === 'CapsLock') {
+      RIGHT.classList.toggle('active');
+    }
+  }
+})
+
+window.addEventListener('keyup', function(el) {
+  for (let i = 0; i < KEYS.length; i++) {
+    if (el.key === KEYS[i].getAttribute('keyValue') || el.key === KEYS[i].getAttribute('lowerCaseValue')) {
+      KEYS[i].classList.remove('active');
+      KEYS[i].classList.add('remove');
+    }
+    if (el.code === 'Space') {
+      SPACE.classList.remove('active');
+      SPACE.classList.add('remove');
+    }
+    if (el.code === 'Delete') {
+      DELETE.classList.remove('active');
+      DELETE.classList.add('remove');
+    }
+    if (el.code === 'ArrowUp') {
+      UP.classList.remove('active');
+      UP.classList.add('remove');
+    }
+    if (el.code === 'ArrowDown') {
+      DOWN.classList.remove('active');
+      DOWN.classList.add('remove');
+    }
+    if (el.code === 'ArrowLeft') {
+      LEFT.classList.remove('active');
+      LEFT.classList.add('remove');
+    }
+    if (el.code === 'ArrowRight') {
+      RIGHT.classList.remove('active');
+      RIGHT.classList.add('remove');
+    }
+    if (el.code === 'Backslash') {
+      BACKSLASH.classList.remove('active');
+      BACKSLASH.classList.add('remove');
+    }
+    if (el.code === 'ControlLeft') {
+      CTRL_LEFT.classList.remove('active');
+      CTRL_LEFT.classList.add('remove');
+    }
+    if (el.code === 'ControlRight') {
+      CTRL_RIGHT.classList.remove('active');
+      CTRL_RIGHT.classList.add('remove');
+    }
+    if (el.code === 'ShiftLeft') {
+      SHIFT_RIGHT.classList.remove('active');
+      SHIFT_RIGHT.classList.remove('remove');
+    }
+    if (el.code === 'ShiftRight') {
+      SHIFT_LEFT.classList.remove('active');
+      SHIFT_LEFT.classList.remove('remove');
+    }
+    if (el.code === 'AltLeft') {
+      ALT_RIGHT.classList.remove('remove');
+      ALT_RIGHT.classList.remove('active');
+    }
+    if (el.code === 'AltRight') {
+      ALT_LEFT.classList.remove('active');
+      ALT_LEFT.classList.remove('remove');
+    }
+  }
+})
